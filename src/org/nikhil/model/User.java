9da768e7bae7f835c8998c8ae1deea2161d4d92b
Nikhil <nnnikhilj@gmail.com>
2014-07-14 20:55:09 +0530

    Project adding

---
+++ src/org/nikhil/model/User.java
@@ -0,0 +1,39 @@
+package org.nikhil.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="USERS")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String firstName;
+	private String lastName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
+
+
